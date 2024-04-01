934902b84079413ab8ad1976e870cf62f249d621
Birukoff <106393434+Biruckoff@users.noreply.github.com>
2024-04-01 14:54:38 +0400

    commit1

--- main.py
+++ main.py
@@ -7,16 +7,27 @@ from strawberry.asgi import GraphQL
 from typing import List, Optional
 from aio_pika import connect_robust, Message, DeliveryMode
 import aioredis
+from bson import ObjectId
+
 
 from src.models import Tx, User
 
 cli = motor.motor_asyncio.AsyncIOMotorClient('localhost', 27017)
 
 db = cli.yat
-txs = db.tx
-users = db.users
+tx_collection = db.txs
+users_collection = db.users
 asks = db.asks
 votes = db.votes
+
+@strawberry.experimental.pydantic.type(model=User, all_fields=True)
+class GraphUser:
+    addr: str
+    name: Optional[str]
+    cover: Optional[str]
+    desc: Optional[str]
+    sign: str
+
 
 @strawberry.experimental.pydantic.type(model=Tx)
 class GraphTx:
@@ -27,59 +38,186 @@ class GraphTx:
     sign: Optional[str]
     hash: strawberry.auto
     msg: strawberry.auto
+    
+    
+async def fetch_all_users():
+    users = await users_collection.find().to_list(None)
+    graph_users = [
+        GraphUser(
+            addr=user["addr"],
+            name=user["name"],
+            cover=user["cover"],
+            desc=user["desc"],
+            sign=user["sign"]
+        )
+        for user in users
+    ]
+    return graph_users
 
-@strawberry.experimental.pydantic.type(model=User, all_fields=True)
-class GraphUser:
-    ...
+
+async def fetch_all_tx():
+    txs = await tx_collection.find().to_list(None)
+    graph_tx = [
+        GraphTx(
+            credit=tx["credit"],
+            debit=tx["debit"],
+            amount=tx["amount"],
+            time=tx["time"],
+            sign=tx["sign"],
+            hash=tx["hash"],
+            msg=tx["msg"]
+            
+        )
+        for tx in txs
+    ]
+    return graph_tx
+
+async def fetch_byid_tx(tx_id):
+    obj_id = ObjectId(tx_id)
+    txs = await tx_collection.find_one({'_id': obj_id})
+    print(txs)
+    graph_tx = GraphTx(
+        credit=txs.get("credit"),
+        debit=txs.get("debit"),
+        amount=txs.get("amount"),
+        time=txs.get("time"),
+        sign=txs.get("sign"),
+        hash=txs.get("hash")
+        )
+    return graph_tx 
+
+async def fetch_byuser_tx(addr):
+    txs_cr = await tx_collection.find({'credit': addr}).to_list(None)
+    txs_db = await tx_collection.find({'debit': addr}).to_list(None)
+    txs_cr.extend(txs_db)
+    graph_tx = []
+    if txs_cr:
+        graph_tx = [
+            GraphTx(
+                credit=tx["credit"],
+                debit=tx["debit"],
+                amount=tx["amount"],
+                time=tx["time"],
+                sign=tx["sign"],
+                hash=tx["hash"],
+                msg=tx["msg"]
+                
+            )
+            for tx in txs_cr
+        ]
+    return graph_tx
+
+async def fetch_all_user_contacts(addr):
+    
+    txs_cr = await tx_collection.find({'credit': addr}).to_list(None)
+    txs_db = await tx_collection.find({'debit': addr}).to_list(None)
+    all_users = []
+    
+    for tx in txs_cr:
+        debit = tx.get("debit")
+        
+        users = await users_collection.find({'addr': debit}).to_list(None)
+        
+        if users:
+            all_users.extend(users)
+    for tx in txs_db:
+        credit = tx.get("credit")
+        
+        users = await users_collection.find({'addr': credit}).to_list(None)
+        
+        if users:
+            all_users.extend(users)
+    
+    graph_users = [
+        GraphUser(
+            addr=user["addr"],
+            name=user["name"],
+            cover=user["cover"],
+            desc=user["desc"],
+            sign=user["sign"]
+        )
+        for user in all_users
+    ]
+    print(txs_cr)
+    return graph_users
+
 
 async def load_balance(addr):
     #ok = await redis.set("key", "value")
     #assert ok
-    b = await redis.get(addr)
-    print(int(b))
-    return int(b)
+    #b = await redis.get(addr)
+    #print(int(b))
+    #return int(b)
+    txs_cr = await tx_collection.find({'credit': addr}).to_list(None)
+    txs_db = await tx_collection.find({'debit': addr}).to_list(None)
+    total = 0
+    for tx in txs_db:
+        total = total +tx.get("amount")
+    for tx in txs_cr:
+        total = total -tx.get("amount")
+    return total
 
-async def load_user(addr, name, cover, desc, sign):
-    print(addr, name, cover, desc, sign)
-    return None
+async def load_user(addr, name, cover, desc, sign)->List[GraphUser]:
+    query = {}
+    if addr:
+        query['addr'] = addr
+    if name:
+        query['name'] = name
+    if cover:
+        query['cover'] = cover
+    if desc:
+        query['desc'] = desc
+    if sign:
+        query['sign'] = sign
+    
+    users = await users_collection.find(query).to_list(None)
+    graph_users = [
+        GraphUser(
+            addr=user["addr"],
+            name=user["name"],
+            cover=user["cover"],
+            desc=user["desc"],
+            sign=user["sign"]
+        )
+        for user in users
+    ]
+    return graph_users
+
+
 
 async def load_tx(amount, addr, msg, time, skip, limit) -> List[GraphTx]:
-    if amount >= 0: amount_direction = '$gt'
-    else: amount_direction = '$lt'; amount = -amount
-    
-    if time >= 0: time_direction = '$gt'
-    else: time_direction = '$lt'; time = -amount
-
-    q = {
-        'amount': {amount_direction: amount},
-        'time': {time_direction: time},
-    }
-
-    if msg != None:
-        if msg == True: q['msg'] = {'$ne': 'null'}
-        else: q['msg'] = 'null'
-
+    query = {}
+    if amount is not 0:
+        query['amount'] = amount
     if addr:
-        q['$or'] = [{'credit': addr}, {'debit': addr}]
+        query['$or'] = [{'credit': addr}, {'debit': addr}]
+    if msg is not '':
+        query['msg'] = msg
+    if time is not 0:
+        query['time'] = time
     
-    print(q)
-
-    return [
-        GraphTx.from_pydantic(Tx(**t)) for t in await txs.find({
-            '$query': q,
-            '$orderby': {'_id': -1}
-            })
-            .skip(skip)
-            .limit(limit)
-            .to_list(None)
+    txs = await tx_collection.find(query).skip(skip).limit(limit).to_list(None)
+    graph_txs = [
+        GraphTx(
+            credit=tx["credit"],
+            debit=tx["debit"],
+            amount=tx["amount"],
+            time=tx["time"],
+            sign=tx["sign"],
+            hash=tx.get("hash"),
+            msg=tx.get("msg")
+        )
+        for tx in txs
     ]
+    
+    return graph_txs
 
 @strawberry.type
 class Query:
     @strawberry.field
     async def tx(
         self,
-        msg: bool = None,
+        msg: str = '',
         addr: str = '',
         amount: int = 0,
         time: int = 0,
@@ -96,7 +234,7 @@ class Query:
         cover: str = '',
         desc: str = '',
         sign: str = ''
-    ) -> None:
+    ) -> List[GraphUser]:
         return await load_user(addr, name, cover, desc, sign)
 
     @strawberry.field
@@ -105,6 +243,26 @@ class Query:
         addr: str = ''
     ) -> float:
         return await load_balance(addr)
+
+    @strawberry.field
+    async def get_all_users(self) -> List[GraphUser]:
+        return await fetch_all_users()
+
+    @strawberry.field
+    async def get_all_TX(self) -> List[GraphTx]:
+        return await fetch_all_tx()
+
+    @strawberry.field
+    async def get_TX_by_id(self, _id: str) -> GraphTx:
+        return await fetch_byid_tx(_id)
+
+    @strawberry.field
+    async def get_TX_by_user(self, addr: str) -> List[GraphTx]:
+        return await fetch_byuser_tx(addr)
+
+    @strawberry.field()
+    async def get_all_user_contacts(self, addr: str) -> List[GraphUser]:
+        return await fetch_all_user_contacts(addr)
 
 
 schema = strawberry.Schema(query=Query)
--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from pydantic import BaseModel, PositiveInt
-
 # Things
+
 
 class User(BaseModel):
     addr: str
